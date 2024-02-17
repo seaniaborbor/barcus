689379af1fc0ef3e8cf7a66644bac89a5d0edafd
seaniaborbor <tarnueatalx@gmail.com>
2024-02-17 00:53:11 -0800

    last update from desktop at home

--- app/Views/public/index.php
+++ app/Views/public/index.php
@@ -2,28 +2,103 @@
 
 <?=$this->section('main')?>
 
+<!-- this is the carousel section -->
     <div class="container-fluid">
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="<?=base_url('imgs/8349010.jpg')?>" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="<?=base_url('imgs/5467015.jpg')?>" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="<?=base_url('imgs/8349010.jpg')?>" class="d-block w-100" alt="...">
+          <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="<?=base_url('imgs/8349010.jpg')?>" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="<?=base_url('imgs/5467015.jpg')?>" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="<?=base_url('imgs/8349010.jpg')?>" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+      </div>
+   </div>
+  <!-- carousel section ends --> 
+
+  <!-- welcome section -->
+
+  <section class="py-5">
+    <div class="container">
+    <div class="row">
+
+      <div class="col-12 text-center py-5">
+        <h3 class="text-uppercase">Welcome To Barcus Group of Agencies</h3>
+        <p>
+          "We're your premier destination for comprehensive solutions in travel, education, MOE, sanitation, design, insurance, financial services, security, branding, and beyond. With a global footprint and a clientele exceeding 100, our expertise spans the spectrum of industries, ensuring tailored solutions to meet your every need. Explore our diverse offerings and embark on a journey of excellence with Bacus Group today."
+        </p>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card rounded-4  rounded shadow-sm mainCard">
+          <div class="card-body text-center">
+            <center>
+          <img src="<?=base_url('imgs/icon/globe.png')?>"  class=" iconImg">
+            </center>
+            <h5>Globally Active</h5>
+            <p>
+              Join forces with a dynamic partner that operates on a global scale. With our extensive reach and network, we transcend borders to bring you unparalleled opportunities and solutions tailored to your needs, no matter where you are in the world.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card rounded shadow-sm rounded-4 mainCard">
+          <div class="card-body text-center">
+            <center>
+              <img src="<?=base_url('imgs/icon/customer.png')?>" class=" iconImg">
+            </center>
+            <h5>100+ Clients</h5>
+            <p>
+              Our track record speaks volumes. Trusted by over 100 clients worldwide, we have a proven history of delivering results and exceeding expectations. Partner with us and become part of our ever-growing family of satisfied clientele.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+       <div class="card rounded shadow-sm rounded-4 mainCard">
+         <div class="card-body text-center">
+            <center>
+            <img src="<?=base_url('imgs/icon/trowel.png')?>" class=" iconImg">
+          </center>
+          <h5>100% Satisfaction Guarantee:</h5>
+          <p>
+            Your satisfaction is our top priority. We stand behind our work with unwavering confidence, offering a 100% satisfaction guarantee on all our services. Rest assured, we're committed to your success and will go above and beyond to ensure you're delighted with the outcomes.
+          </p>
+         </div>
+       </div>  
+      </div>
+
+        <div class="col-md-3">
+          <div class="card rounded shadow-sm rounded-4 mainCard">
+            <div class="card-body text-center">
+              <center>
+              <img src="<?=base_url('imgs/icon/affordable.png')?>" class=" iconImg">
+              </center>
+            <h5>Affordable Prices</h5>
+            <p>
+              Unlock premium services without breaking the bank. At Bacus Group, we believe in providing exceptional value at competitive prices. Benefit from our cost-effective solutions and enjoy peace of mind knowing you're getting the best bang for your buck.
+            </p>
+            </div>
+          </div>
+        </div>
+
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    </div>
+  </section>
 
 <?=$this->endSection()?>
--- app/Views/public/partials/common/layout.php
+++ app/Views/public/partials/common/layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <style>
     	a {
       font-size:14px;
@@ -22,6 +23,11 @@
         text-align:center
       }
     }
+
+    .iconImg{
+      max-width: 100px !important;
+      margin:auto !important;
+    }
   </style>
   
   </head>
@@ -29,25 +35,32 @@
     
 
   <div class="superNav border-bottom py-2 bg-light">
+
       <div class="container">
         <div class="row">
           <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 centerOnMobile">
+            <span class="me-3"> <span class="badge bg-light border border-danger rounded-circle">
+              <i class="bi bi-telephone-inbound mt-3 mb-3 text-danger"></i>
+            </span> <strong>1-800-123-1234</strong></span>
+            <span class="d-none d-lg-inline-block d-md-inline-block d-sm-inline-block d-xs-none me-3">
+              <span class="badge bg-light border border-danger rounded-circle">
+              <i class="bi bi-envelope mt-3 mb-3 text-danger"></i>
+            </span>
+              <strong>info@somedomain.com</strong></span>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 d-none d-lg-block d-md-block-d-sm-block d-xs-none text-end">
             <select  class="me-3 border-0 bg-light">
               <option value="en-us">EN-US</option>
             </select>
-            <span class="d-none d-lg-inline-block d-md-inline-block d-sm-inline-block d-xs-none me-3"><strong>info@somedomain.com</strong></span>
-            <span class="me-3"><i class="fa-solid fa-phone me-1 text-warning"></i> <strong>1-800-123-1234</strong></span>
-          </div>
-          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 d-none d-lg-block d-md-block-d-sm-block d-xs-none text-end">
-            <span class="me-3"><i class="fa-solid fa-truck text-muted me-1"></i><a class="text-muted" href="#">Shipping</a></span>
             <span class="me-3"><i class="fa-solid fa-file  text-muted me-2"></i><a class="text-muted" href="#">Policy</a></span>
           </div>
         </div>
       </div>
     </div>
+
     <nav class="navbar navbar-expand-lg bg-white sticky-top navbar-light p-3 shadow-sm">
       <div class="container">
-        <a class="navbar-brand" href="#"><i class="fa-solid fa-shop me-2"></i> <strong>GEAR SHOP</strong></a>
+        <a class="navbar-brand" href="#"><img src="<?=base_url('imgs/sitelogo.png')?>" style="max-width: 200px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -60,13 +73,13 @@
           </div>
         </div>
         <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-          <div class="ms-auto d-none d-lg-block">
+         <!--  <div class="ms-auto d-none d-lg-block">
             <div class="input-group">
               <span class="border-warning input-group-text bg-warning text-white"><i class="fa-solid fa-magnifying-glass"></i></span>
               <input type="text" class="form-control border-warning" style="color:#7a7a7a">
               <button class="btn btn-warning text-white">Search</button>
             </div>
-          </div>
+          </div> -->
           <ul class="navbar-nav ms-auto ">
             <li class="nav-item">
               <a class="nav-link mx-2 text-uppercase active" aria-current="page" href="#">Offers</a>
@@ -88,8 +101,8 @@
             <li class="nav-item">
               <a class="nav-link mx-2 text-uppercase" href="#"><i class="fa-solid fa-cart-shopping me-1"></i> Cart</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2 text-uppercase" href="#"><i class="fa-solid fa-circle-user me-1"></i> Account</a>
+            <li class="nav-item btn btn-danger ptn-sm rounded-0 px-0">
+              <a class="nav-link mx-2 text-uppercase text-white mx-0  " href="#"><i class="bi bi-person me-1"></i> Account</a>
             </li>
           </ul>
         </div>
